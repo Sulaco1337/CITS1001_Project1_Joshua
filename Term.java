93eaaf4590abba489e34042884de7f2e4096ad0d
Joshua Patterson <sulaco@pinball.ucc.gu.uwa.edu.au>
2015-04-15 17:25:12 +0800

    1.0.3 WORKING

--- Term.java
+++ Term.java
@@ -15,13 +15,13 @@ public class Term
         coefficient = c;
         exponent = e;
     }
-    
+
     // returns the coefficient
     public double getCoefficient()
     {
         return coefficient;
     }
-    
+
     // returns the exponent
     public int getExponent()
     {
@@ -32,32 +32,32 @@ public class Term
     // see the sample file for the layout required 
     public String display()
     { String result = "";
-    if(coefficient==0){
-    return "+ 0";}
-    else if(coefficient%1==0){
-    //is int
-     if(coefficient == 1 && exponent != 0){
-   result = result + "+ ";}
-   else if(coefficient == -1 && exponent != 0){
-   result = result + "- ";}
-   else if(coefficient == 1 && exponent != 1){
-   result = result + "+ x";}
-   else if(coefficient == -1 && exponent != 1){
-   result = result + "- x";}
-   //For a good time, call 0400455687
-    else if (coefficient < 0){ result = result + "- " + (String.valueOf((int) -1 *  coefficient));}
-   else result = result + "+ " + String.valueOf((int) coefficient);
-    }
-    else{
-    //is double
-    if (coefficient < 0){ result = result + "- " + (String.valueOf(-1 *  coefficient));}
-   else result = result + "+ " + String.valueOf(coefficient);
-    }
-   
-if (exponent == 1){ result = result + "x";}
-   else if (exponent == 0){} 
-   else {result	= result + "x^" + String.valueOf(exponent); 
-   }
-       return result;
+        if(coefficient==0){
+            return "+ 0";}
+        else if(coefficient%1==0){
+            //is int
+            if(coefficient == 1 && exponent != 0){
+                result = result + "+ ";}
+            else if(coefficient == -1 && exponent != 0){
+                result = result + "- ";}
+            else if(coefficient == 1 && exponent != 1){
+                result = result + "+ x";}
+            else if(coefficient == -1 && exponent != 1){
+                result = result + "- x";}
+            //For a good time, call 0400455687
+            else if (coefficient < 0){ result = result + "- " + (String.valueOf((int) -coefficient));}
+            else result = result + "+ " + String.valueOf((int) coefficient);
+        }
+        else{
+            //is double
+            if (coefficient < 0){ result = result + "- " + (String.valueOf(-1 *  coefficient));}
+            else result = result + "+ " + String.valueOf(coefficient);
+        }
+
+        if (exponent == 1){ result = result + "x";}
+        else if (exponent == 0){} 
+        else {result	= result + "x^" + String.valueOf(exponent); 
+        }
+        return result;
     }
 }
